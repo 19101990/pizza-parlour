9c2adf35aa7236a96b36a96ebd0f6d10c067ea88
19101990 <e19101990@gmail.com>
2018-06-17 01:32:04 +0200

    create pizza_menu.py

---
+++ pizza_menu.py
@@ -0,0 +1,9 @@
+pizzas = {'MARGHERITA': 11.99,
+          'GREEK PIZZA': 12.99,
+          'PEPPERONI': 15.99,
+          'HAWAIIAN': 13.99,
+          'NEW YORK STYLE': 15.99,
+          'SICILIAN': 16.99,
+          'BBQ CHICKEN': 14.99}
+
+print(pizzas['PEPPERONI'])
