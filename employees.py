1f44b5c8fdee49b06151c8a8fafb6f79444ff664
19101990 <e19101990@gmail.com>
2018-06-17 01:30:48 +0200

    update employees.py

--- employees.py
+++ employees.py
@@ -1,7 +1,10 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
+from person import Person
+from customer import Customer
+from pizza_menu import pizzas
+import time
 
+
+# ------- Employee ------- #
 
 class Employee(Person):
     def __init__(self, name, salary = 0):
@@ -9,24 +12,50 @@ class Employee(Person):
         self.salary = salary
 
 
-class Chef(Employee):
-    def __init__(self, name, salary = 0):
-        super().__init__(name, salary)
-
-    def cook(self):
-        print('>', self.name, 'is making the sauce')
-        print('>', self.name, 'is making the pizza')
-
+# ------- SERVER ------- #
 
 class Server(Employee):
     def __init__(self, name, salary = 0):
         super().__init__(name, salary)
 
-    def take_order(self):
-        print(self.name, ': Hi!, my name is ', self.name, '.')
+    def introduce(self):
+        print('Hi! My name is {}. What\'s your name?'.format(self.name))
+        global name_option
+        name_option = input('')
+        c = Customer(name_option)
+        print('Nice to meet you, {}!'.format(c.name))
+        time.sleep(1)
+        print('Here\'s our menu:')
+        time.sleep(2)
 
-    def serve(self):
-        print(self.name, ': Here\'s your order.')
+    def show_pizza_menu(self):
+        print('\n')
+        for i in pizzas:
+            print(i, pizzas[i])
+
+    def offer_drinks(self):
+        pass
+
+    def serve_drinks(self):
+        pass
+
+    def serve_pizza(self):
+        pass
+
+    def bring_check(self):
+        pass
 
 
+# ------- COOK ------- #
+
+class Cook(Employee):
+    def __init__(self, name, salary = 0):
+        super().__init__(name, salary)
+
+    def make_pizza(self):
+        pass
+
+s = Server("Matt")
+s.introduce()
+s.show_pizza_menu()
 
