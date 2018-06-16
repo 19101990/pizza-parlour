a3ca22285e578787c9269efd53312b8a790b842a
19101990 <e19101990@gmail.com>
2018-06-17 01:31:17 +0200

    update equipment.py

--- equipment.py
+++ equipment.py
@@ -1,10 +1,3 @@
 class Owen:
-    def bake(self):
-        print("> baking pizza in the owen")
-
-class Mixer:
-    def make_dough(self):
-        print("> preparing pizza dough")
-
-
-
+    def bake_pizza(self):
+        pass
